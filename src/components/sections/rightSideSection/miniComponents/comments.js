7a307efbcb4298a7da55b80d88b2e65e2dedff14
Duy Khánh Nguyễn <ndkhanh1996@gmail.com>
2023-07-25 05:36:33 +0700

    complete world comment

--- src/components/sections/rightSideSection/miniComponents/comments.js
+++ src/components/sections/rightSideSection/miniComponents/comments.js
@@ -4,23 +4,35 @@ import Button from '@mui/joy/Button';
 import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import Textarea from '@mui/joy/Textarea';
-import IconButton from '@mui/joy/IconButton';
-import Menu from '@mui/joy/Menu';
-import MenuItem from '@mui/joy/MenuItem';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import FormatBold from '@mui/icons-material/FormatBold';
-import FormatItalic from '@mui/icons-material/FormatItalic';
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
-import Check from '@mui/icons-material/Check';
 import {useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 export default function ExampleTextareaComment(props) {
     const [worldComment, setWorldComment] = useState(false);
+    const [comment, setComment] = useState('');
+    const [isCommentAdded, setIsCommentAdded] = useState(false);
     let account
-    if (props){
+    if (props) {
         account = props.session.accountInfo
     }
+
+    const addComment = () => {
+        const newWorldComment = {
+            account: { id: account.id },
+            content: comment,
+            commentTime: new Date().toISOString()
+        };
+        axios
+            .post('http://localhost:8080/worldComment/add', newWorldComment)
+            .then((response) => {
+                console.log('World comment added successfully:', response.data);
+                setIsCommentAdded(true);
+            })
+            .catch((error) => {
+                console.error('Failed to add world comment:', error);
+            });
+    };
 
     useEffect(() => {
         fetch('http://localhost:8080/worldComment/')
@@ -31,23 +43,21 @@ export default function ExampleTextareaComment(props) {
                     throw new Error('worldComment');
                 }
             })
-            .then(data => setWorldComment(data))
+            .then((data) => {
+                setWorldComment(data);
+                setIsCommentAdded(false);
+            })
             .catch(error => {
                 console.error(error);
             });
-    }, []);
+    }, [isCommentAdded]);
 
-    console.log(account)
 
-    const [italic, setItalic] = React.useState(false);
-    const [fontWeight, setFontWeight] = React.useState('normal');
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const italic = false;
+    const fontWeight = 'normal';
     return (
         <FormControl>
             <h1 style={{paddingLeft: 10, color: "#2980b9"}}>Chat thế giới</h1>
-
-
-
             {worldComment.result && worldComment.result.length > 0 && (
                 worldComment.result.map((item, index) => (
                     <Box
@@ -60,19 +70,18 @@ export default function ExampleTextareaComment(props) {
                             marginBottom: '10px',
                         }}
                     >
-                        <Typography sx={{ color: 'blue' }}>{item.account.name}</Typography>
+                        <Typography sx={{color: 'blue'}}>{item.account.name}</Typography>
                         <Typography>{item.content}</Typography>
                     </Box>
                 ))
             )}
 
-
-
-
-            <FormLabel sx={{ marginLeft: '10px', }}>Your comment</FormLabel>
+            <FormLabel sx={{marginLeft: '10px'}}>Your comment</FormLabel>
             <Textarea
                 placeholder="Type something here…"
                 minRows={3}
+                value={comment}
+                onChange={(e) => setComment(e.target.value)} // Hàm xử lý khi nội dung của ô input thay đổi
                 endDecorator={
                     <Box
                         sx={{
@@ -84,48 +93,7 @@ export default function ExampleTextareaComment(props) {
                             flex: 'auto',
                         }}
                     >
-                        <IconButton
-                            variant="plain"
-                            color="neutral"
-                            onClick={(event) => setAnchorEl(event.currentTarget)}
-                        >
-                            <FormatBold/>
-                            <KeyboardArrowDown fontSize="md"/>
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={() => setAnchorEl(null)}
-                            size="sm"
-                            placement="bottom-start"
-                            sx={{'--ListItemDecorator-size': '24px'}}
-                        >
-                            {['200', 'normal', 'bold'].map((weight) => (
-                                <MenuItem
-                                    key={weight}
-                                    selected={fontWeight === weight}
-                                    onClick={() => {
-                                        setFontWeight(weight);
-                                        setAnchorEl(null);
-                                    }}
-                                    sx={{fontWeight: weight}}
-                                >
-                                    <ListItemDecorator>
-                                        {fontWeight === weight && <Check fontSize="sm"/>}
-                                    </ListItemDecorator>
-                                    {weight === '200' ? 'lighter' : weight}
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                        <IconButton
-                            variant={italic ? 'soft' : 'plain'}
-                            color={italic ? 'primary' : 'neutral'}
-                            aria-pressed={italic}
-                            onClick={() => setItalic((bool) => !bool)}
-                        >
-                            <FormatItalic/>
-                        </IconButton>
-                        <Button sx={{ml: 'auto'}}>Send</Button>
+                        <Button sx={{ml: 'auto'}} onClick={addComment}>Send</Button>
                     </Box>
                 }
                 sx={{
